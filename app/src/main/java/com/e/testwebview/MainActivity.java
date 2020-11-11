bff375819f36643a42c62af7dac96126b87d08bb
FCAlfred <41550868+FCAlfred@users.noreply.github.com>
2020-11-11 12:29:14 -0600

    New Branch
    new branch created

--- app/src/main/java/com/e/testwebview/MainActivity.java
+++ app/src/main/java/com/e/testwebview/MainActivity.java
@@ -9,28 +9,15 @@ import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
-WebView webView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        webView = findViewById(R.id.webView);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://www.youtube.com/");
-        
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
 
     }
 
-    @Override
-    public void onBackPressed() {
-        if(webView.canGoBack()){
-            webView.goBack();
-        }else{
-            super.onBackPressed();
-        }
-    }
+
 }
