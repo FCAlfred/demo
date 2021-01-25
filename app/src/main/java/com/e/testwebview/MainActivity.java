cc97331e50126d265a90d1d570ace9aa372b2d29
FCAlfred <41550868+FCAlfred@users.noreply.github.com>
2021-01-24 18:35:04 -0600

    minor Changes

--- app/src/main/java/com/e/testwebview/MainActivity.java
+++ app/src/main/java/com/e/testwebview/MainActivity.java
@@ -3,21 +3,13 @@ package com.e.testwebview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
     }
-
-
 }
